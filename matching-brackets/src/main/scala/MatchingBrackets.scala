0b1116fdcbd83f085e5f5f5c46a1d261ff4addfb
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2019-10-29 16:47:51 +0100

    Matching Brackets, take 2

--- matching-brackets/src/main/scala/MatchingBrackets.scala
+++ matching-brackets/src/main/scala/MatchingBrackets.scala
@@ -5,13 +5,21 @@ object MatchingBrackets {
 
   @tailrec
   def isPaired(brackets: List[Char], stack: List[Char]): Boolean = brackets match {
-    case '[' :: tail => isPaired(tail, '[' :: stack)
-    case ']' :: tail => stack.nonEmpty && stack.head == '[' && isPaired(tail, stack.tail)
-    case '(' :: tail => isPaired(tail, '(' :: stack)
-    case ')' :: tail => stack.nonEmpty && stack.head == '(' && isPaired(tail, stack.tail)
-    case '{' :: tail => isPaired(tail, '{' :: stack)
-    case '}' :: tail => stack.nonEmpty && stack.head == '{' && isPaired(tail, stack.tail)
-    case _ :: tail => isPaired(tail, stack)
-    case Nil => stack.isEmpty
+    case head :: tail =>
+      if (Seq('[', '(', '{').contains(head))
+        isPaired(tail, head :: stack)
+      else if (Seq(']', ')', '}').contains(head))
+        stack.nonEmpty && stack.head == inverse(head) && isPaired(tail, stack.tail)
+      else
+        isPaired(tail, stack)
+    case Nil =>
+      stack.isEmpty
+  }
+
+  private def inverse(bracket: Char): Char = bracket match {
+    case ']' => '['
+    case ')' => '('
+    case '}' => '{'
+    case x => x
   }
 }
