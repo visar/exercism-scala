6e4cb26a3d1f0d5cdcd611960b6365998ae663c8
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2019-11-03 14:26:18 +0100

    Food Chain

---
+++ food-chain/src/main/scala/FoodChain.scala
@@ -0,0 +1,86 @@
+import scala.annotation.tailrec
+
+object FoodChain {
+  def recite(startVerse: Int, endVerse: Int): String = {
+    @tailrec
+    def recite(startVerse: Int, endVerse: Int, acc: String): String = {
+      if (startVerse > endVerse) {
+        acc
+      }
+      else {
+        val verse =
+          startVerse match {
+            case 1 => """I know an old lady who swallowed a fly.
+                        |I don't know why she swallowed the fly. Perhaps she'll die.
+                        |
+                        |""".stripMargin
+
+            case 2 => """I know an old lady who swallowed a spider.
+                        |It wriggled and jiggled and tickled inside her.
+                        |She swallowed the spider to catch the fly.
+                        |I don't know why she swallowed the fly. Perhaps she'll die.
+                        |
+                        |""".stripMargin
+
+            case 3 => """I know an old lady who swallowed a bird.
+                        |How absurd to swallow a bird!
+                        |She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+                        |She swallowed the spider to catch the fly.
+                        |I don't know why she swallowed the fly. Perhaps she'll die.
+                        |
+                        |""".stripMargin
+
+            case 4 => """I know an old lady who swallowed a cat.
+                        |Imagine that, to swallow a cat!
+                        |She swallowed the cat to catch the bird.
+                        |She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+                        |She swallowed the spider to catch the fly.
+                        |I don't know why she swallowed the fly. Perhaps she'll die.
+                        |
+                        |""".stripMargin
+
+            case 5 => """I know an old lady who swallowed a dog.
+                        |What a hog, to swallow a dog!
+                        |She swallowed the dog to catch the cat.
+                        |She swallowed the cat to catch the bird.
+                        |She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+                        |She swallowed the spider to catch the fly.
+                        |I don't know why she swallowed the fly. Perhaps she'll die.
+                        |
+                        |""".stripMargin
+
+            case 6 => """I know an old lady who swallowed a goat.
+                        |Just opened her throat and swallowed a goat!
+                        |She swallowed the goat to catch the dog.
+                        |She swallowed the dog to catch the cat.
+                        |She swallowed the cat to catch the bird.
+                        |She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+                        |She swallowed the spider to catch the fly.
+                        |I don't know why she swallowed the fly. Perhaps she'll die.
+                        |
+                        |""".stripMargin
+
+            case 7 => """I know an old lady who swallowed a cow.
+                        |I don't know how she swallowed a cow!
+                        |She swallowed the cow to catch the goat.
+                        |She swallowed the goat to catch the dog.
+                        |She swallowed the dog to catch the cat.
+                        |She swallowed the cat to catch the bird.
+                        |She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+                        |She swallowed the spider to catch the fly.
+                        |I don't know why she swallowed the fly. Perhaps she'll die.
+                        |
+                        |""".stripMargin
+
+            case 8 => """I know an old lady who swallowed a horse.
+                        |She's dead, of course!
+                        |
+                        |""".stripMargin
+          }
+        recite(startVerse + 1, endVerse, acc ++ verse)
+      }
+    }
+
+    recite(startVerse, endVerse, "")
+  }
+}
---
+++ food-chain/src/test/scala/FoodChainTest.scala
@@ -0,0 +1,171 @@
+import org.scalatest.{FunSuite, Matchers}
+
+/** @version 2.1.0 */
+class FoodChainTest extends FunSuite with Matchers {
+
+  test("fly") {
+    FoodChain.recite(1, 1) should be(
+      """I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+""")
+  }
+
+  test("spider") {
+    FoodChain.recite(2, 2) should be(
+      """I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+""")
+  }
+
+  test("bird") {
+    FoodChain.recite(3, 3) should be(
+      """I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+""")
+  }
+
+  test("cat") {
+    FoodChain.recite(4, 4) should be(
+      """I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+""")
+  }
+
+  test("dog") {
+    FoodChain.recite(5, 5) should be(
+      """I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+""")
+  }
+
+  test("goat") {
+    FoodChain.recite(6, 6) should be(
+      """I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+""")
+  }
+
+  test("cow") {
+    FoodChain.recite(7, 7) should be(
+      """I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+""")
+  }
+
+  test("horse") {
+    FoodChain.recite(8, 8) should be(
+      """I know an old lady who swallowed a horse.
+She's dead, of course!
+
+""")
+  }
+
+  test("multiple verses") {
+    FoodChain.recite(1, 3) should be(
+      """I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+""")
+  }
+
+  test("full song") {
+    FoodChain.recite(1, 8) should be(
+      """I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a horse.
+She's dead, of course!
+
+""")
+  }
+}
