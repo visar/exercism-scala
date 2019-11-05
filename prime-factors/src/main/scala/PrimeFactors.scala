eda51c3599b697bf934a55f0e1e48d5bcf7dab3b
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2019-11-05 01:34:48 +0100

    Prime Factors

---
+++ prime-factors/src/main/scala/PrimeFactors.scala
@@ -0,0 +1,27 @@
+import scala.annotation.tailrec
+
+object PrimeFactors {
+  def factors(input: Long): List[Long] = {
+    @tailrec
+    def factors(queue: List[Long], list: List[Long]): List[Long] = {
+      if (queue.isEmpty)
+        list.reverse
+      else if (isPrime(queue.head)) {
+        factors(queue.tail, queue.head :: list)
+      }
+      else {
+        val pair: List[Long] = (2 to Math.sqrt(queue.head).toInt).filter(queue.head % _ == 0).take(1).flatMap(x => List(x, queue.head / x)).toList
+        factors(queue.tail ++ pair, list)
+      }
+    }
+
+    input match {
+      case 1 => List()
+      case _ => factors(List(input), List())
+    }
+  }
+
+  def isPrime(input: Long): Boolean = {
+    (2 to Math.sqrt(input).toInt).forall(input % _ != 0)
+  }
+}
