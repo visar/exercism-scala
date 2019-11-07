79364ea991916bf3d20caf596f8fea549f105338
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2019-11-07 10:53:27 +0100

    Pascal's Triangle

---
+++ pascals-triangle/src/main/scala/PascalsTriangle.scala
@@ -0,0 +1,18 @@
+object PascalsTriangle {
+  def rows(row_number: Int): List[List[Int]] = {
+    if (row_number <= 0)
+      List()
+    else
+      row_number match {
+        case 1 => List(List(1))
+        case n =>
+          val previous = rows(n - 1)
+          val lastRow = previous.last
+
+          previous :+
+            (0 :: lastRow, lastRow :+ 0).zipped.map {
+              (a, b) => a + b
+            }
+      }
+  }
+}
