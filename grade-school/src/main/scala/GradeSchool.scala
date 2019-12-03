7f5497e35348659ab79911018c3a1e3fc5b6babb
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2019-12-03 21:02:31 +0100

    Refactor Grade School

--- grade-school/src/main/scala/GradeSchool.scala
+++ grade-school/src/main/scala/GradeSchool.scala
@@ -1,17 +1,16 @@
 import scala.collection.SortedMap
 
 class GradeSchool {
-  type DB = SortedMap[Int, Seq[String]]
+  type DB = Map[Int, Seq[String]]
   var DataBase: SortedMap[Int, Seq[String]] = SortedMap[Int, Seq[String]]()
 
-  def add(name: String, g: Int): DB = {
+  def add(name: String, g: Int): Unit = {
     DataBase += (g -> (DataBase.getOrElse(g, Seq()) ++ Seq(name)))
-    DataBase
   }
 
-  def db: DB = DataBase
+  def db: DB = DataBase.toMap
 
   def grade(g: Int): Seq[String] = DataBase.getOrElse(g, Seq())
 
-  def sorted: DB = DataBase.mapValues(_.sorted)
+  def sorted: DB = DataBase.mapValues(_.sorted).toMap
 }
