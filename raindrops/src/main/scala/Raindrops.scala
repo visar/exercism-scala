d983fce30e3dfaa7ec782fad8c2cc16cdf45be63
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2020-09-15 15:26:13 +0200

    Raindrops take 2

--- raindrops/src/main/scala/Raindrops.scala
+++ raindrops/src/main/scala/Raindrops.scala
@@ -1,17 +1,16 @@
 object Raindrops {
-  def isFactorOf(number: Int, factor: Int): Boolean = number % factor == 0
 
-  def convert(n: Int): String = {
-    n match {
-      case n if isFactorOf(n, 3) && isFactorOf(n, 5) && isFactorOf(n, 7) => "PlingPlangPlong"
-      case n if isFactorOf(n, 3) && isFactorOf(n, 5) => "PlingPlang"
-      case n if isFactorOf(n, 3) && isFactorOf(n, 7) => "PlingPlong"
-      case n if isFactorOf(n, 5) && isFactorOf(n, 7) => "PlangPlong"
-      case n if isFactorOf(n, 3) => "Pling"
-      case n if isFactorOf(n, 5) => "Plang"
-      case n if isFactorOf(n, 7) => "Plong"
-      case n => n.toString
-    }
+  implicit class Factor(number: Int) {
+    def isFactorOf(factor: Int): Boolean = number % factor == 0
+  }
+
+  def convert(implicit n: Int): String = {
+    var str: String = ""
+    n isFactorOf 3
+    if (n isFactorOf 3) str += "Pling"
+    if (n isFactorOf 5) str += "Plang"
+    if (n isFactorOf 7) str += "Plong"
+    if (str != "") str else n.toString
   }
 }
 
