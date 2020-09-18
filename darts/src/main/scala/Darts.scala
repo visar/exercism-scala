47e2a10f41721fbeadac69d8a496cc2aa22a0ddb
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2020-09-18 17:50:20 +0200

    Darts

---
+++ darts/src/main/scala/Darts.scala
@@ -0,0 +1,10 @@
+object Darts {
+  def score(x: Double, y: Double): Int = {
+    val distance = math.sqrt(x * x  + y * y)
+
+    if (distance == 0) 10
+    else if (distance <= 5) 5
+    else if (distance <= 10) 1
+    else 0
+  }
+}
