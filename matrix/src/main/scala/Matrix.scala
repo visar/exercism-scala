587fb3ef9df8e4ae4bc52a99ba0f3986a43c7b45
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2020-09-21 16:56:59 +0200

    Matrix

---
+++ matrix/src/main/scala/Matrix.scala
@@ -0,0 +1,11 @@
+trait MatrixTrait {
+  def rows: Array[Array[Int]]
+  def columns: Array[Array[Int]]
+}
+
+case class Matrix(string: String) extends MatrixTrait {
+  val rows: Array[Array[Int]]        = string.split("\n").map(s => s.split(" ").map(i => i.toInt))
+  val columns: Array[Array[Int]]     = rows.transpose
+  def row(index: Int): Array[Int]    = rows(index)
+  def column(index: Int): Array[Int] = columns(index)
+}
