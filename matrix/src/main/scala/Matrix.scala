17737304655d110992a503c6753a8ceafb364719
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2020-09-24 00:38:05 +0200

    Matrix 2

--- matrix/src/main/scala/Matrix.scala
+++ matrix/src/main/scala/Matrix.scala
@@ -4,7 +4,7 @@ trait MatrixTrait {
 }
 
 case class Matrix(string: String) extends MatrixTrait {
-  val rows: Array[Array[Int]]        = string.split("\n").map(s => s.split(" ").map(i => i.toInt))
+  val rows: Array[Array[Int]]        = string.split("\n").map(_.split(" ").map(_.toInt))
   val columns: Array[Array[Int]]     = rows.transpose
   def row(index: Int): Array[Int]    = rows(index)
   def column(index: Int): Array[Int] = columns(index)
