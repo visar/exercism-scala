3697f7244e3520b5480217c84b0fc2448a18930b
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2021-10-03 23:33:08 +0200

    Armstrong numbers

--- armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
+++ armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -2,20 +2,10 @@ import scala.annotation.tailrec
 
 object ArmstrongNumbers {
   def isArmstrongNumber(input: Int): Boolean = {
-    val digs           = digits(input)
+    val digs           = input.toString
     val numberOfDigits = digs.length
-    val checkSum       = digs.fold(0)(_ + Math.pow(_, numberOfDigits).toInt)
+    val checkSum       = digs.foldLeft(0)((acc, ch) => acc + Math.pow(ch.asDigit, numberOfDigits).toInt)
 
     checkSum == input
-  }
-
-  def digits(input: Int): List[Int] = {
-    @tailrec
-    def digits(input: Int, digs: List[Int]): List[Int] = {
-      if (input == 0) digs
-      else digits(input / 10, (input % 10) :: digs)
-    }
-
-    digits(input, List())
   }
 }
