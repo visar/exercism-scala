08fee5cdc68e0aae85e380562b6edeca293d8a6d
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2022-08-31 16:15:14 +0200

    Add updates

--- simple-linked-list/src/main/scala/SimpleLinkedList.scala
+++ simple-linked-list/src/main/scala/SimpleLinkedList.scala
@@ -8,23 +8,25 @@ sealed trait SimpleLinkedList[+T] {
 }
 
 object Empty extends SimpleLinkedList[Nothing] {
-  override def isEmpty: Boolean                                = true
-  override def value                                           = throw new NoSuchElementException
-  override def add[T](item: T): SimpleLinkedList[T] = NonEmpty(item, Empty)
-  override def next: SimpleLinkedList[Nothing]                 = throw new NoSuchElementException
-  override def reverse: SimpleLinkedList[Nothing]              = this
-  override def toSeq: Seq[Nothing]                             = Nil
+  override def isEmpty: Boolean                     = true
+  override def value                                = throw new NoSuchElementException
+  override def add[T](item: T): SimpleLinkedList[T] = SimpleLinkedList(item)
+  override def next: SimpleLinkedList[Nothing]      = throw new NoSuchElementException
+  override def reverse: SimpleLinkedList[Nothing]   = this
+  override def toSeq: Seq[Nothing]                  = Seq.empty
 }
 
-case class NonEmpty[T](value: T, next: SimpleLinkedList[T]) extends SimpleLinkedList[T] {
+case class Cons[T](head: T, tail: SimpleLinkedList[T]) extends SimpleLinkedList[T] {
+  override val value: T                                     = head
+  override val next: SimpleLinkedList[T]                    = tail
   override def isEmpty: Boolean                             = false
-  override def add[T1 >: T](item: T1): SimpleLinkedList[T1] = NonEmpty(value, next.add(item))
-  override def reverse: SimpleLinkedList[T]                 = next.reverse.add(value)
-  override def toSeq: Seq[T]                                = value +: next.toSeq
+  override def add[T1 >: T](item: T1): SimpleLinkedList[T1] = Cons(head, tail.add(item))
+  override def reverse: SimpleLinkedList[T]                 = tail.reverse.add(head)
+  override def toSeq: Seq[T]                                = head +: tail.toSeq
 }
 
 object SimpleLinkedList {
   def apply[T](): SimpleLinkedList[T]              = Empty
   def apply[T](elements: T*): SimpleLinkedList[T]  = fromSeq(elements)
-  def fromSeq[T](seq: Seq[T]): SimpleLinkedList[T] = seq.foldLeft(SimpleLinkedList[T]())((list, el) => list.add(el))
+  def fromSeq[T](seq: Seq[T]): SimpleLinkedList[T] = seq.foldRight(SimpleLinkedList[T]())(Cons(_, _))
 }
